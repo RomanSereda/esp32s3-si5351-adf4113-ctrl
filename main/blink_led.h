d8083feb0c991865cd3d2df420c416ec75ca03e5
RomanSereda <i.sereda.roman@gmail.com>
2024-02-19 22:01:21 +0200

    add blink_led.

---
+++ main/blink_led.h
@@ -0,0 +1,5 @@
+#pragma once
+#define CONFIG_BLINK_LED_GPIO 1
+
+void blink_led(void);
+void configure_led(void);
--- main/main.c
+++ main/main.c
@@ -1,7 +1,11 @@
 
+#include "blink_led.h"
 #include "tinyusb_msc_storage.h"
 
 void app_main(void)
 {
+    configure_led();
     const esp_partition_t* partition = tinyusb_msc_storage();
+
+    blink_led();
 }
